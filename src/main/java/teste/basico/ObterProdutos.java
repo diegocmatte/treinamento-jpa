e16bde932b437b3fbb3421ea51d1aee301094ef2
Diego Matté <diegocmatte@gmail.com>
2021-09-02 18:23:36 -0300

    DAO com generics

---
+++ src/main/java/teste/basico/ObterProdutos.java
@@ -0,0 +1,34 @@
+package teste.basico;
+
+import modelo.basico.Produto;
+import modelo.basico.ProdutoDAO;
+
+import java.util.List;
+
+public class ObterProdutos {
+
+    public static void main(String[] args) {
+
+        ProdutoDAO produtoDAO = new ProdutoDAO();
+        List<Produto> produtos = produtoDAO.obterTodos();
+
+        for(Produto p: produtos){
+            System.out.println("ID: " + p.getId() + "; Nome: " + p.getNome() + ";");
+        }
+
+        double precoTotal = produtos.stream()
+                .map(produto -> produto.getPreco())
+                .reduce(0.0, (t,p) -> t +p)
+                .doubleValue();
+
+        double precoTotalOtimizado = produtos.stream()
+                .map(Produto::getPreco)
+                .reduce(0.0, Double::sum);
+
+        System.out.println("O valor total é: R$" + precoTotal);
+        System.out.println("O valor total otimizado é: R$" + precoTotalOtimizado);
+
+        produtoDAO.fechar();
+
+    }
+}
